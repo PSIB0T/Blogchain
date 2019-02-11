7d053eab7c4306777c4815cc7c2fc82bae2e1130
Arvind Narayanan <arvindonarayanan@gmail.com>
2019-02-11 06:25:24 +0530

    Update: Integrate peernode and box

--- src/components/app.js
+++ src/components/app.js
@@ -1,8 +1,9 @@
 'use strict'
 
 const React = require('react')
-const IPFS = require('ipfs')
+let {createIPFSobj} = require('./../utils/IpfsUtil')
 const OrbitDB = require('orbit-db');
+const Box = require('3box');
 
 const stringToUse = 'hello world from webpacked IPFS'
 
@@ -10,12 +11,18 @@ class App extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
+      isLoading: true,
+      metamaskOff: false,
+      name: "",
+      publicAddress: "",
+      ethereumProvider: null,
       id: null,
       version: null,
       protocol_version: null,
       added_file_hash: null,
       added_file_contents: null,
       receiveurl: "",
+      ipfs: null,
       orbitdb: null,
       db: null,
       value: null
@@ -34,34 +41,80 @@ class App extends React.Component {
     })
   }
 
+  getEthAccounts(web3Provider) {
+    return new Promise((resolve, reject) => {
+      web3Provider.eth.getAccounts((err, res) => {
+        if (err)
+          reject(err)
+        else
+          resolve(res)
+      })
+    })
+  }
+
+  async create() {
+
+    return createIPFSobj(false, "QmXMrn2WKrWp9xdLWBKu2V8dth5R3emp6MPhrgiFRdfgBZ",
+                          "CAASpgIwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDpOsOBI06wuq4Z0pGEifwHE234PhB6y4+g4ukdzQ3Z1Oe7iyCihuECJW3wr3ic3MWUWF/lpbc6RMTOjqgmTA0Y2BFSQs3AifpuUmcoeAUPu1b6rn2J34OiVNE6ZZJ/oVS7M6e+zLjmxqloB4RLXjZNdj2cCLGVYO27VCgvEyjLFtxCY+gnFRYP2XwQ2U0v6s9ZqEeHIEbrFY8e0Qm7GE/pp5cT4N8ZPJKVY7lL0NEqieRYpBNMG9a7tbmDNMzXDuFqmTmZvR8PvTKgFSwuTFfQ4TyzTnLffStmADj3MmH/IafKmDuqcf9j6K9Maoq2Cm0UYvCqlF5HHY0PjWCtYHGvAgMBAAE="
+                          ,"CAASqQkwggSlAgEAAoIBAQDpOsOBI06wuq4Z0pGEifwHE234PhB6y4+g4ukdzQ3Z1Oe7iyCihuECJW3wr3ic3MWUWF/lpbc6RMTOjqgmTA0Y2BFSQs3AifpuUmcoeAUPu1b6rn2J34OiVNE6ZZJ/oVS7M6e+zLjmxqloB4RLXjZNdj2cCLGVYO27VCgvEyjLFtxCY+gnFRYP2XwQ2U0v6s9ZqEeHIEbrFY8e0Qm7GE/pp5cT4N8ZPJKVY7lL0NEqieRYpBNMG9a7tbmDNMzXDuFqmTmZvR8PvTKgFSwuTFfQ4TyzTnLffStmADj3MmH/IafKmDuqcf9j6K9Maoq2Cm0UYvCqlF5HHY0PjWCtYHGvAgMBAAECggEAJg29emONYk6DO5lcvthYs7ra1Yci3zY5cBG14XPkj6cqTDJOF9aT/eqnfn7S2h03a5Xjx697Ltmrltw12VHNTUFryhU2RvT8VGXDiRnUEZYKggpYV1eNvb3DCZkrBwIGtiiedQYoIRdG6r9XOYjcqVTihoPwnaPcBhWcHNoubLw1ZK7HcIuFJaBNnpewJawuzn6tlFoYyk2GpJiySc/XZiQ17glD7qks4qwCuG6EguLHVEOWZbb8G4ja9bknESrz8EbA28cFN9ZA7pJ3J2u+sPT41VH/uA9YYVCVWlyTgg6DOEpS8YX156ZVmrJzU8Lstgu4qcWN7NZ5JwUA0qTQkQKBgQD2Dr/VKhJKVD4O6S8gAU3I0a/iuxom7Fnf7EsZ+FBodBSgQzu730sdwvcneYMhzArrEMpkCo6Mo8AAGBI5J1WJ2BUdalxYLV0yUhPN2rDI7tZYJjbvhXsVHbpwQoFc5BgldJyekKg+3w68m5z4NqSMK+n7ZUrKy6tnPYUyNQPkuQKBgQDyp1G27YWqBUMyw/3O6wtVbJH/Ksosb0uwrcNGoVUUHAVBmbhQpNxTz6ZB/fEno3Z4NtqyvoOlP4LYEqu3Ip2p8ozkp0blmpbCchMjoFETSISEfkbFf7appYwVGkAeKBWd2UL/9NNds0Rb9T+sik8ZdQsHSU3kiWTWOqZjAUOlpwKBgQDU4/gIOAlxNCgpgInceCRTz1ENq/K5oJ82RdzI0HbJmT3LV8CUEWONkYWEKvdRQ5hiv7lNC3FAr+FMc4c42haBWGsUc1UImd9bzXYDEucIQtVDmXTIkkXnBDyuKmyA8X0O0zE7ZE+dbiLy/vS2MZj3Cnu8nmwywDtB7RGUWhhPSQKBgQC3FGUD9uv9nuDXoaaTAyZW+oahFoDZy9M457HJTZpqhMW2fCvjtK+EEMzpR5c3CA8vnnudlz6uZF/tdRDYKSb5/cYuEsPcIqoS5YzpOWFSgklxyfDNvJFuKDisB1VPK4E7ypvRkMAVF1fBZiX3oZcSpwt+IOHDR6KsPC4jzuViSwKBgQDOBZuR2unBip7oBhrdNRiUZ0+NL+lj0DVDjC4aVuSoQ80pXAW7qzRdJaMcDe2Xo7Kmy+a4w7fS+T7wPA8Vu4R3X2xbjP4I4eCurwvipXzDgdGrIIzHhbW/RkaKQcCq/SaqfkcqeMWe3ZRN2o8ZlpyFoEf2AwtiAFD+/ouPn6Vptw==")
+      .then(({ipfs}) => {
+        return this.setStatePromise({ipfs})
+      })
+  }
+
+  async getProfile() {
+    let self = this;
+    let globalNode, address, ethereumProvider;
+    try {
+      ethereumProvider = window.web3.currentProvider
+      address = await this.getEthAccounts(window.web3)
+      if (address.length === 0) 
+        throw "Please log into metamask"
+      address = address[0].toString()
+      console.log("Address is " + address)
+    } catch(err) {
+      console.log(err)
+      this.setState({
+        isLoading: false,
+        metamaskOff: true
+      })
+      console.log(err)
+      return;
+    }
+    let profile = await Box.getProfile(address)
+    return self.setStatePromise({
+      name: profile.name,
+      ethereumProvider,
+      publicAddress: address
+    })
+  }
+
+  async initializeNode() {
+    Box.openBox(this.state.publicAddress, this.state.ethereumProvider)
+        .then((box) => {
+          box.onSyncDone(async () => {
+            console.log("Synced with box!")
+            
+          })
+        })
+  }
+
   componentDidMount () {
     const self = this
-    let node
-
-    create()
-
-    function create () {
-      // Create the IPFS node instance
-
-      node = new IPFS({
-          EXPERIMENTAL: {pubsub: true},
-          config: {
-            Addresses: {
-              Swarm: [
-                '/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star'
-              ]
-            }
-          }
+    self.getProfile()
+        .then(() => {
+          this.initializeNode()
+        })
+    self.create(true)
+        .then(() => {
+          self.state.ipfs.once('ready', () => {
+            console.log('IPFS node is ready')
+            ops()
+          })
         })
 
-      node.once('ready', () => {
-        console.log('IPFS node is ready')
-        ops()
-      })
-    }
-
     function ops () {
-      node.id((err, res) => {
+      self.state.ipfs.id((err, res) => {
         if (err) {
           throw err
         }
@@ -70,7 +123,7 @@ class App extends React.Component {
           version: res.agentVersion,
           protocol_version: res.protocolVersion
         }).then(async () => {
-          let orbitdb = new OrbitDB(node)
+          let orbitdb = new OrbitDB(self.state.ipfs)
           let db = await orbitdb.keyvalue('myfirstdb')
           await db.load();
           let value = db.get("value")
---
+++ src/utils/IpfsUtil.js
@@ -0,0 +1,66 @@
+import IPFS from 'ipfs'
+import PeerId from 'peer-id';
+
+function createPeer(newNode, id, pubKey, privKey) {
+    return new Promise((resolve, reject) => {
+        if (newNode === true) {
+            PeerId.create((err, res) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(res)
+                }                
+            })
+        } else {
+            PeerId.createFromJSON({
+                id,
+                pubKey,
+                privKey
+            }, (err, res) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(res)
+                }
+                
+            })
+        }
+
+    })
+
+}
+
+
+export function createIPFSobj (newNode, id, pubKey, privKey) {
+    // Create the IPFS node instance
+
+    return new Promise((resolve, reject) => {
+        let repo = "./repo-"
+        createPeer(newNode, id, pubKey, privKey)
+            .then((peerNode) => {
+                let tempVar = JSON.parse(JSON.stringify(peerNode, null, 1))
+                repo += tempVar.id
+                console.log(tempVar)
+                let ipfs = new IPFS({
+                    repo,
+                    EXPERIMENTAL: {pubsub: true},
+                    init: {
+                        privateKey: tempVar.privKey
+                    },
+                    config: {
+                        Addresses: {
+                            Swarm: [
+                                '/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star'
+                            ]
+                        }
+                    }
+                })
+                resolve({ipfs})
+            })
+
+        
+
+    })
+
+    
+}
